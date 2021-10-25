c1fab26c5e9ca2082b675e054494f686bf313dd4
sa30h <satishrjk07@gmail.com>
2021-10-25 22:51:43 +0530

    new

---
+++ chat/routing.py
@@ -0,0 +1,13 @@
+from django.urls import re_path
+
+from . import consumers
+
+# websocket_urlpatterns = [
+#     re_path(r'peer[12]/', consumers.ChatConsumer.as_asgi()),
+#     re_path(r'', consumers.ChatConsumer.as_asgi()),
+# ]
+
+websocket_urlpatterns=[
+    re_path(r'peer[12]/', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$',consumers.ChatConsumer.as_asgi()),
+]
